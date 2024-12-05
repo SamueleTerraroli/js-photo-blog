cdca1516af46a4298c16135fdc38eec048a66650
SamueleTerraroli <terraroli99@gmail.com>
2024-12-05 13:05:20 +0100

    feat: add printer and catch error

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,32 @@
-/*
-                <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center my-5 ">
-                    <div class="s-card d-flex flex-column ">
-                        <img class="pin" src="assets/img/pin.svg" alt="">
-
-                        <div class="img-section">
-                            <img src="assets/img/400.svg" alt="">
-                        </div>
-                        <div class="text-section">
-                            <p class="image-description my-3"></p>
-
-                        </div>
-
-                    </div>
-                </div>
-*/
+const displayedAlbum = document.querySelector('.album-section');
+const errorMsg = document.querySelector('.error-message');
 
 const api_url='https://jsonplaceholder.typicode.com/photos?_limit=6';
 axios.get(api_url)
     .then(res => {
-    console.log(res.data);
-    
-})
+        res.data.forEach(album =>{
+            const{title , url} = album;
+            displayedAlbum.innerHTML+= `
+            <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center my-5 ">
+                    <div class="s-card d-flex flex-column ">
+                        <img class="pin" src="assets/img/pin.svg" alt="">
+
+                        <div class="img-section">
+                            <img src="${url}" alt="${title}">
+                        </div>
+                        <div class="text-section">
+                            <p class="image-description my-3">${title}</p>
+
+                        </div>
+
+                    </div>
+                </div>`;
+        });
+    })
+    .catch(error => { 
+        errorMsg.classList.remove('d-none');
+        errorMsg.innerHTML = `Errore durante la richiesta API: ${error.message}`;
+    });
+
+
 
