9706718016ec4170e92471deaaa25c31c10b781d
SamueleTerraroli <terraroli99@gmail.com>
2024-12-06 10:27:23 +0100

    fix: capitalize title

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,15 @@
 const displayedAlbum = document.querySelector('.album-section');
 const errorMsg = document.querySelector('.error-message');
 
+errorMsg.innerHTML = '';
+
 const api_url='https://jsonplaceholder.typicode.com/photos?_limit=6';
 axios.get(api_url)
     .then(res => {
         res.data.forEach(album =>{
             const{title , url} = album;
-            displayedAlbum.innerHTML+= `
-            <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center my-5 ">
-                    <div class="s-card d-flex flex-column ">
-                        <img class="pin" src="assets/img/pin.svg" alt="">
-
-                        <div class="img-section">
-                            <img src="${url}" alt="${title}">
-                        </div>
-                        <div class="text-section">
-                            <p class="image-description my-3">${title}</p>
-
-                        </div>
-
-                    </div>
-                </div>`;
+            printAlbum(title,url);
+            
         });
     })
     .catch(error => { 
@@ -28,5 +17,27 @@ axios.get(api_url)
         errorMsg.innerHTML = `Errore durante la richiesta API: ${error.message}`;
     });
 
+function printAlbum(title , url){
+    const capitalizedTitle = capitalizeFirstLetter(title);
+    displayedAlbum.innerHTML+= `
+        <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center my-5 ">
+                <div class="s-card d-flex flex-column ">
+                    <img class="pin" src="assets/img/pin.svg" alt="">
+
+                    <div class="img-section">
+                        <img src="${url}" alt="${capitalizedTitle}">
+                    </div>
+                    <div class="text-section">
+                        <p class="image-description my-3">${capitalizedTitle}</p>
+
+                    </div>
+
+                </div>
+            </div>`;
+}
+
+function capitalizeFirstLetter(title){
+    return title.charAt(0).toUpperCase();
+}
 
 
